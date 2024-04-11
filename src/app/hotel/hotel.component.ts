8cfb480db41af1ecd3ab982a4571b78386d29fb9
Leon Wiese <l.wiese@reply.de>
2024-04-11 10:27:37 +0200

    Add getHotel to service

--- src/app/hotel/hotel.component.ts
+++ src/app/hotel/hotel.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hotel } from '../models';
 import { UpperCasePipe } from '@angular/common';
+import { HotelsService } from '../services/hotels.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-hotel',
@@ -11,6 +13,36 @@ import { UpperCasePipe } from '@angular/common';
   templateUrl: './hotel.component.html',
   styleUrl: './hotel.component.css',
 })
-export class HotelComponent {
+export class HotelComponent implements OnInit {
   hotel?: Hotel;
+
+  constructor(
+    private route: ActivatedRoute,
+    private hotelService: HotelsService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    const hotelId = this.route.snapshot.paramMap.get('id');
+    if (hotelId) {
+      this.getHotel(hotelId);
+    }
+  }
+
+  getHotel(hotelId: string) {
+    this.hotelService.getHotel(hotelId)
+      .subscribe(hotel => {
+        this.hotel = hotel;
+      });
+  }
 }
+
+
+
+
+
+
+
+
+
+
--- src/app/services/hotels.service.ts
+++ src/app/services/hotels.service.ts
@@ -1,18 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Hotel } from '../models';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HotelsService {
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getHotels(): Observable<Hotel[]> {
     return this.http.get<Hotel[]>(`${this.apiUrl}/hotels`);
   }
+
+  getHotel(id: string): Observable<Hotel | undefined> {
+    return this.http.get<Hotel>(this.apiUrl + '/hotels/' + id)
+      .pipe(
+        catchError(this.handleError('getHotel', undefined)),
+      );
+  }
+
+  private handleError<T>(operation: string, result: T) {
+    return (error: unknown): Observable<T> => {
+      // TODO: send this error to remote logging infrastructure
+      console.error(`${operation} http request failed`, error);
+
+      return of(result);
+    };
+  }
 }
+
+
+
+
+
+
+
+
+
+
