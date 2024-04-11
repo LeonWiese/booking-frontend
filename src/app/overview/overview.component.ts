cc2b19ec61de84951f8373136f40894d40ef89f6
Leon Wiese <l.wiese@reply.de>
2024-04-11 11:43:23 +0200

    Added "add hotel" form

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -2,18 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { HotelsService } from '../services/hotels.service';
 import { Hotel } from '../models';
 import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
   imports: [
     RouterLink,
+    ReactiveFormsModule,
   ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css',
 })
 export class OverviewComponent implements OnInit {
   hotels: Hotel[] = [];
+
+  addHotelForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required
+    ]),
+    location: new FormControl('', [
+      Validators.required
+    ]),
+  });
+
+  showAddHotelForm = false;
 
   constructor(private hotelsService: HotelsService) {
   }
@@ -27,5 +40,10 @@ export class OverviewComponent implements OnInit {
       .subscribe(hotels => {
         this.hotels = hotels;
       });
+  }
+
+  saveHotel() {
+    const newHotel = this.addHotelForm.value;
+    console.log('newHotel:', newHotel);
   }
 }
